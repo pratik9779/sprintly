187ac71edec0a6accd47262b35d0148912a79afe
pratik <pbsawant2000@gmail.com>
2024-11-30 22:15:34 +0530

    restructuring folder structure

--- project-manager-backend/app/config/db.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    CONNECTION_LIMIT: process.env.CONNECTION_LIMIT,
-    HOST: process.env.DB_HOST,
-    USER: process.env.DB_USERNAME,
-    PASSWORD: process.env.DB_PASSWORD,
-    DB: process.env.DB
-};
